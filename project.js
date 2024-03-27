92e8288c28b4f3ac438c3d403dbe76deca4ed4a7
Opticpeak <opticpeak@gmail.com>
2024-03-27 18:24:15 +0100

    added spinning the wheel and determine the win

--- project.js
+++ project.js
@@ -2,19 +2,44 @@
 // 2. choose bet per lines
 // 3. choose lines
 // 4. calculate bet amount
-
 // 5. spin the wheels
 // 6. determine win
+
+//TODO
+ //1.changing balance
+ //2. what happens if user has no money left
+ //3. play again loop and try to search for break restrictions
+
 // 7. play again
+
+// 8. improve console messages
+// 9. try to change row and cols
 
 
 
 // creating prompt method to collect user inputs
 const prompt = require("prompt-sync")()
 
-//constant values
+// constant values
 let bankBalance = 1000
 let gameBalance = 0
+const rows = 3
+const cols = 3
+
+// Slotsymbols
+const SYMBOLS_AMOUNT = {    // symbols per wheel
+    A: 12,
+    B: 8,
+    C: 4,
+    D: 2
+}
+const SYMBOL_MULTIPLIER = { // multiplier per line
+    A: 1,
+    B: 2,
+    C: 4,
+    D: 8
+}
+
 
 
 
@@ -60,12 +85,96 @@ const calculate_total_bet = (betPerLine, lines) => {
     return betPerLine * lines
 }
 
+// 5.1 spin the wheels
+const spinWheels = () => {
+    const slotWheels = [[], [], []]
+    const allSymbols = []
+    for (const [symbol, amount] of Object.entries(SYMBOLS_AMOUNT)) {
+        for (let i = 0; i < amount; i++) {
+            allSymbols.push(symbol)
+        }
+    }
+
+    for (let i = 0; i < cols; i++) {
+        const leftSymbols = [...allSymbols]
+        for (let j = 0; j < rows; j++) {
+            let posSymbol = Math.floor(Math.random() * leftSymbols.length)
+            slotWheels[i] += leftSymbols[posSymbol]
+            leftSymbols.splice(posSymbol, 1)
+        }
+    }
+    return slotWheels
+}
+
+// 5.2 print the slot
+const print_spin = (slotWheels) => {
+    for (let i = 0; i < rows; i++) {
+        let output = []
+        for (let j = 0; j < cols; j++) {
+            if (j >= cols - 1) {
+                output += slotWheels[j][i]
+            } else {
+                output += slotWheels[j][i]
+                output += " | "
+            }
+        }
+        console.log(output)
+    }
+}
+
+// 6. determine the potential win
+const determine_win = (spinnedWheels, linesAmount, betPerLine) => {
+    // initial 0 winnings
+    let winnings = 0
+
+    // transpose wheels to get started with comparing lines related to winnings
+    const lines = [[], [], []]
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            lines[i].push(spinnedWheels[j][i])
+        }
+    }
+
+    // determine which potential multiplier player gets for chosen lines
+    const potentialMultiplierSymbols = []
+    for (let i = 0; i < linesAmount; i++) {
+        potentialMultiplierSymbols.push(lines[i][0])
+    }
+
+    // checking if all elements of current line are the same
+    for (let i = 0; i < linesAmount; i++ ) {
+        let allSame = true
+        for (let j = 1; j < cols; j++) {
+            if (lines[i][j] != lines[i][0]) {
+                allSame = false
+            }
+        }
+        // adding the reward to win related to win on line x
+        if (allSame == true) {
+            let currentMultiplierSmybol = potentialMultiplierSymbols[i]
+            winnings += betPerLine * SYMBOL_MULTIPLIER[currentMultiplierSmybol]
+        }
+    }
+    // output
+    console.log("You won: $" + winnings + " in total")
+    return winnings
+}
+
+
 
 
 
 // "game loop"
+/*
 gameBalance += deposit()
 let betAmount = bet_per_line()
 let betLines = choose_lines()
 let totalBet = calculate_total_bet(betAmount, betLines)
 console.log("Your total bet is: $" + totalBet)
+
+ */
+
+let spin = spinWheels()
+console.log(spin)
+print_spin(spin)
+determine_win(spin, 2, 5)
